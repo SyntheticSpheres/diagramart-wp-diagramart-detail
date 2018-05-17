f1d6b82266afe6708e28ea2035fa9679030c4026
Matej Jellus <juffalow@juffalow.com>
2018-05-17 02:32:56 +0200

    Change function names

--- wp-diagramart-detail.php
+++ wp-diagramart-detail.php
@@ -11,27 +11,27 @@ Version: 1.0.0
 
 define( 'WPDAD_PLUGIN', __FILE__ );
 
-function htmlCode($id) {
+function wpdad_html_code($id) {
   return "<div id=\"diagramart-detail\"></div><script>document.addEventListener(\"DOMContentLoaded\", function() {DiagramArtDetail({selector:'diagramart-detail', id:$id});});</script>";
 }
 
-function dad_shortcode($atts) {
+function wpdad_shortcode($atts) {
   $params = shortcode_atts(
     array(
       'id' => 0,
     ), $atts );
 
   $id = (int) $params['id'];
-	return htmlCode($id);
+	return wpdad_html_code($id);
 }
 
 function register_shortcodes(){
-   add_shortcode( 'diagramart-detail', 'dad_shortcode' );
+   add_shortcode( 'diagramart-detail', 'wpdad_shortcode' );
 }
 
 add_action( 'init', 'register_shortcodes');
 
-function wpdal_plugin_url( $path = '' ) {
+function wpdad_plugin_url( $path = '' ) {
 	$url = plugins_url( $path, WPDAD_PLUGIN );
 
 	if ( is_ssl() && 'http:' == substr( $url, 0, 5 ) ) {
@@ -41,19 +41,19 @@ function wpdal_plugin_url( $path = '' ) {
 	return $url;
 }
 
-function wpdal_do_enqueue_scripts() {
-	wpdal_enqueue_scripts();
-	wpdal_enqueue_styles();
+function wpdad_do_enqueue_scripts() {
+	wpdad_enqueue_scripts();
+	wpdad_enqueue_styles();
 }
 
-function wpdal_enqueue_scripts() {
-	wp_enqueue_script( 'diagramart-detail', wpdal_plugin_url( 'includes/js/diagramart.js' ));
-	do_action( 'wpdal_enqueue_scripts' );
+function wpdad_enqueue_scripts() {
+	wp_enqueue_script( 'diagramart-detail', wpdad_plugin_url( 'includes/js/diagramart.js' ));
+	do_action( 'wpdad_enqueue_scripts' );
 }
 
-function wpdal_enqueue_styles() {
-	wp_enqueue_style( 'diagramart-detail', wpdal_plugin_url( 'includes/css/diagramart.css' ));
-	do_action( 'wpdal_enqueue_styles' );
+function wpdad_enqueue_styles() {
+	wp_enqueue_style( 'diagramart-detail', wpdad_plugin_url( 'includes/css/diagramart.css' ));
+	do_action( 'wpdad_enqueue_styles' );
 }
 
-add_action( 'wp_enqueue_scripts', 'wpdal_do_enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'wpdad_do_enqueue_scripts' );
